d46e7f98a733087ff3e8269cefd9cec33ebb03b3
Maksim Ashmetev <maksim_ashmetev@epam.com>
2019-01-22 23:26:12 +0300

    Verify the existence articles

--- client/src/reducers/data.js
+++ client/src/reducers/data.js
@@ -5,6 +5,13 @@ const initialState = {
   isFetching: false
 };
 
+function validResult(articles) {
+ if (articles.length) {
+   return { result: true, articles }
+ }
+ return { result: false, articles: [] }
+}
+
 export const getData = (queryString) => ({ type: GET_DATA, payload: queryString });
 export const getDataSuccess = () => ({ type: GET_DATA_SUCCESS});
 
@@ -12,9 +19,9 @@ function data(state = initialState, action) {
   console.log('action', action);
     switch (action.type) {
         case GET_DATA:
-          return { isFetching: true };
+          return { isFetching: true, articles: [] };
         case GET_DATA_SUCCESS:
-          return { isFetching: false };
+          return { isFetching: false, ...validResult(action.payload) };
         default:
           return state;
       }
