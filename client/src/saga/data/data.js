93da53e3e34bdf4fbadf36bc90c43d6bf9913ec4
Maksim Ashmetev <maksim_ashmetev@epam.com>
2019-01-22 23:05:39 +0300

    Fix query string

--- client/src/saga/data/data.js
+++ client/src/saga/data/data.js
@@ -1,13 +1,15 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
 import * as Api from '../../api/Api';
 
-export function* getData(): Generator<*, *, *> {
+export function* getData(action): Generator<*, *, *> {
+  console.log('action', action);
+  console.log(`/api/search?q=${action.payload}`);
   try {
-    const response = yield call(Api.getJSON, '/api/search?q=obama', {});
+    const response = yield call(Api.getJSON, `/api/search?q=${action.payload}`);
     console.log('response', response);
     yield put({
       type: 'GET_DATA_SUCCESS',
-      payload: response
+      payload: response.data.response.docs
     });
   } catch (error) {
     yield console.log(error);
